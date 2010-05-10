264455075a659d00ca0918846ba368e8698e8b44
Nully <nully.nl@gmail.com>
2010-05-10 19:08:27 +0900

    add plugin methods

--- js/jquery.monogusa.js
+++ js/jquery.monogusa.js
@@ -28,59 +28,156 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+(function(){
+    Array.prototype.inArray = function(value) {
+        for(var i in this) {
+            if(this[i] === value) {
+                return true;
+            }
+        }
+        return false;
+    }
+})();
+
 (function($){
-    // monogusa JS clauser global Object
+    var allowedPlugins = [
+        "tab", "stripe", "highlight", "scroller", "accordion"
+    ];
+    $.fn.monogusa = function(method, options) {
+        if(!allowedPlugins.inArray(method)) {
+            throw $.monogusa.riseError("not allowd method '"+ method +"' !");
+        }
+
+        try{
+            return $.monogusa[method].call(this, options);
+        }
+        catch(e) {
+            throw $.monogusa.riseError(e);
+        }
+    }
+
+
+    // monogusa Object
     $.monogusa = {
-        version: 1.0,
-        fn: {
-            getVersion: function() {return $.monogusa.version;},
-            isFunction: $.isFunction,
-            riseError:  function(msg) {throw msg;},
-            hasClass:   function(class_name) {},
-            hasMethod:  function(method) {return $.monogusa.isFunction(method);}
+        accordion: function(options) {
         },
-        cls: {
-            autoover: function() {}
+        /**
+         * simple tab panel
+         * for core UI tab, jQuery UI use it.
+         *
+         * @access pubic
+         * @param  options    Object
+         *  onClick:  on click to tab click callback
+         *  onChange: on change to tab panel callback
+         */
+        tab: function(options) {
+            var settings = $.extend({
+                tab_active_class: "active",
+                onClick: function() {},
+                onChange: function() {}
+            }, options || {});
+
+
+            return this.each(function(i){
+                var $t = $(this),
+                    $li = $("li", $t),
+                    $anchors = $("a", $li),
+                    panelSelectors;
+
+                $li.css({
+                    "float": "left"
+                });
+                $anchors.css({
+                    "border": "1px solid #CECECE",
+                    "padding": "3px",
+                    "margin-right": "5px",
+                    "display": "block"
+                });
+
+
+                $t.addClass("monogusa-tabbox-" + i);
+                $anchors.each(function(){
+                    var selector = $(this).attr("href");
+                    if(panelSelectors) {
+                        panelSelectors = panelSelectors.add(selector);
+                    }
+                    else {
+                        panelSelectors = $(selector);
+                    }
+
+                    $(this).bind("click", function(ev){
+                        panelSelectors.hide();
+                        if($.isFunction(settings.onClick)) {
+                            settings.onClick.call(this, this);
+                        }
+                        $(selector).show();
+                        ev.preventDefault();
+                    });
+                });
+                panelSelectors.css("clear", "both");
+                $anchors.first().trigger("click");
+            });
         },
-        extend: $.extend // extend alias
+        /**
+         * stripe a rows.
+         *
+         * @access public
+         * @param  options    Object
+         *  odd_class:  odd row class
+         *  even_class: even row class
+         */
+        stripe: function(options) {
+            var settings = $.extend({
+                odd_class: "odd",
+                even_class: "even"
+            }, options || {});
+
+            this.each(function(){
+                $(this).find("tr, li").each(function(i){
+                    var cls = (i%2) ? settings.even_class : settings.odd_class;
+                    $(this).addClass(cls);
+                });
+            });
+        },
+        /**
+         * highlight for IE, not supported :hover class.
+         *
+         * @access public
+         * @param  options    Object
+         */
+        highlight: function(options) {
+            var settings = $.extend({
+                hover_class: "monogusa_hover",
+                onHover: function() {},
+                onOut: function() {}
+            }, options || {});
+
+            this.each(function(){
+                $(this).hover(function(){
+                    if($.isFunction(settings.onHover)) {
+                        settings.onHover.call(this);
+                    }
+                    $(this).addClass(settings.hover_class);
+                }, function(){
+                    if($.isFunction(settings.onOut)) {
+                        settings.onOut.call(this);
+                    }
+                    $(this).removeClass(settings.hover_class);
+                });
+            });
+        },
+        /**
+         * RiseError message
+         *
+         * @access public
+         * @param  message    String    error message
+         */
+        riseError: function(message) {
+            if(window.console) {
+                console.log(message);
+            }
+            alert(message);
+        }
     };
-
-
-    // autoover
-    $.fn.autoover = function() {};
-
-
-    // alternate
-    $.fn.alternate = function() {};
-
-
-    // hightlight
-    $.fn.highlight = function() {};
-
-
-    // original thickbox
-    $.fn.thickbox = function() {};
-
-
-    // tabs
-    $.fn.tabs = function() {};
-
-
-    // auto scroll
-    $.fn.scroll = function() {};
-
-
-    // accordion
-    $.fn.accordion = function() {};
-
-
-    // prototype alias
-    $.autoover.fn  = $.autoover.prototype  = {};
-    $.alternate.fn = $.alternate.prototype = {};
-    $.highlight.fn = $.highlight.prototype = {};
-    $.thickbox.fn  = $.thickbox.prototype  = {};
-    $.tabs.fn      = $.tabs.prototype      = {};
-    $.scroll.fn    = $.scroll.prototype    = {};
-    $.accordion.fn = $.accordion.prototype = {};
 })(jQuery);
 
