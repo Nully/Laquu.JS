b6c7c13cea5372bed91a32f23ae6c8fd57e70249
Nully <nully.nl@gmail.com>
2010-06-01 17:12:06 +0900

    create class rollOpacity, rollImage, ticker

--- js/mootools.monogusa.js
+++ js/mootools.monogusa.js
@@ -37,12 +37,82 @@ var monogusa;
 
 
     /**
-     * monogusa Rollover class Object
+     * monogusa Opacity Rollover class Object
+     *
      */
-    var monogusaRollover = new Class({
+    var monogusaRollOpacity = new Class({
         Extends: monogusaAbstract,
+        _options: {
+            opacity: .7,
+            duration: 300,
+            onHover: $empty,
+            onOut: $empty
+        },
         initialize: function(e, o) {
+            var t = this;
             this.parent(e, o);
+            this.getElement().each(function(el, i){
+                el.addEvent("mouseover", function(ev){
+                    t.onHandler(ev);
+                });
+                el.addEvent("mouseout", function(ev){
+                    t.outHandler(ev);
+                });
+            });
+        },
+        onHandler: function(ev) {
+            ev.target.get("tween", {
+                "property": "opacity",
+                "duration": this.getOption("duration")
+            }).start(this.getOption("opacity"));
+            this.getOption("onHover").apply("",arguments);
+        },
+        outHandler: function(ev) {
+            ev.target.get("tween", {
+                "property": "opacity",
+                "duration": this.getOption("duration")
+            }).start(1);
+            this.getOption("onOut").apply("",arguments);
+        }
+    });
+
+
+    /**
+     * monogusa Opacity Rollover class Object
+     *
+     */
+    var monogusaRollImage = new Class({
+        Extends: monogusaAbstract,
+        _options: {
+            suffix: "_on",
+            onHover: $empty,
+            onOut: $empty
+        },
+        initialize: function(e, o) {
+            var t = this;
+            this.parent(e, o);
+            this.getElement().each(function(el, i){
+                el.addEvent("mouseover", function(ev){
+                    t.onHandler(ev);
+                });
+                el.addEvent("mouseout", function(ev){
+                    t.outHandler(ev);
+                });
+            });
+        },
+        onHandler: function(ev) {
+            var src = ev.target.getProperty("src"),
+                tmp = src.split(".");
+            var ext = tmp.pop();
+            src = tmp.join(".") + this.getOption("suffix") + "." + ext;
+            ev.target.setProperty("src", src);
+
+        },
+        outHandler: function(ev) {
+            var el = ev.target;
+            var src = el.getProperty("src");
+            el.setProperty("src", src.replace(new RegExp(this.getOption("suffix")), ""));
+            
         }
     });
 
@@ -52,8 +122,60 @@ var monogusa;
      */
     var monogusaTicker = new Class({
         Extends: monogusaAbstract,
+        _options: {
+            speed: 2,
+            pause: 2,
+            onComplete: $empty,
+            onNext: $empty
+        },
+        current_item: -1,
+        items: [],
+        morpth: null,
         initialize: function(e, o) {
+            if($type(e) == "array")
+                e = e[0];
+
             this.parent(e, o);
+            this.getElement().getParent("div").setStyles({
+                position: "relative"
+            });
+            this.getElement().setStyles({
+                position: "absolute"
+            });
+
+            this.items = this.getElement().getElements("li");
+            this.morph = new Fx.Morph(this.getElement(), {
+                duration: this.getOption("speed") * 1000,
+                onComplete: function() {
+                    var i = (this.current_item === 0) ? this.items.length : this.current_item;
+                    this.items[i - 1].injectInside(this.getElement());
+                    this.getElement().setStyles({
+                        top: 0,
+                        left: 0
+                    });
+                    this.getOption("onComplete").apply("", arguments);
+                }.bind(this)
+            });
+            this.next();
+        },
+        next: function() {
+            ++this.current_item;
+            this.getOption("onNext").call(this.current());
+            this.rewind();
+            var pos = this.items[this.current_item];
+            this.morph.start({
+                top: -pos.offsetTop,
+                left: -pos.offsetLeft
+            });
+            this.next.bind(this).delay(this.getOption("pause") * 1000 + this.getOption("speed") * 1000);
+        },
+        rewind: function() {
+            if(this.items.length <= this.current_item) {
+                this.current_item = 0;
+            }
+        },
+        current: function() {
+            return this.items[this.current_item];
         }
     });
 
@@ -162,7 +284,8 @@ var monogusa;
      *
      */
     var monogusaFn = {
-        "rollover": monogusaRollover,
+        "rollopacity": monogusaRollOpacity,
+        "rollimage": monogusaRollImage,
         "ticker": monogusaTicker,
         "blank": monogusaBlank,
         "accordion": monogusaAccordion,
@@ -211,7 +334,7 @@ var monogusa;
          * @throw
          */
         _isAllowedMethod: function(m) {
-            if($type(monogusaFn[m]) != "function")
+            if($type(monogusaFn[m]) != "class")
                 throw "Mehtod "+ m +" is not defined !";
         },
         /**
@@ -220,7 +343,7 @@ var monogusa;
          * @throw
          */
         _isAllowedElement: function(e) {
-            if($type(el) == undefined)
+            if($type(e) == undefined)
                 throw "HTML Element required !";
         }
     });
