ab5dd8612dbec5dd5d963799d947e45492571687
Nully <nully.nl@gmail.com>
2010-06-02 19:12:00 +0900

    renew classes.

--- js/mootools.monogusa.js
+++ js/mootools.monogusa.js
@@ -1,269 +1,279 @@
 var monogusa;
 (function(){
     /**
-     * monogusa Abstract class Object
-     *
-     */
-    var monogusaAbstract = new Class({
-        _options: {},
-        _element: null,
-        _version: 1.0,
-        initialize: function(el, op) {
-            this.setElement(el);
-            this.setOptions(op);
-        },
-        getVersion: function() {
-            return this._version;
-        },
-        setElement: function(el) {
-            this._element = el;
-        },
-        getElement: function() {
-            return this._element;
-        },
-        setOptions: function(options) {
-            this._options = $merge(options, this._options);
-        },
-        getOptions: function() {
-            return this._options;
-        },
-        setOption: function(n, v) {
-            this._options[n] = v;
-        },
-        getOption: function(n) {
-            return this._options[n];
-        }
-    });
-
-
-    /**
-     * monogusa Opacity Rollover class Object
-     *
+     * monogusa Konami command class Object
      */
     var monogusaRollOpacity = new Class({
-        Extends: monogusaAbstract,
-        _options: {
-            opacity: .7,
-            duration: 300,
-            onHover: $empty,
-            onOut: $empty
-        },
+        Extends: Fx.Tween,
         initialize: function(e, o) {
-            var t = this;
-            this.parent(e, o);
-            this.getElement().each(function(el, i){
-                el.addEvent("mouseover", function(ev){
-                    t.onHandler(ev);
-                });
-                el.addEvent("mouseout", function(ev){
-                    t.outHandler(ev);
-                });
+            this.elements = e;
+            this.options  = $extend(o || {}, {
+                "opacity": .7,
+                "duration": 300,
+                "onComplete": $empty,
+                "onHover": $empty,
+                "onOut": $empty
             });
+
+            this.elements.each(function(e, i){
+                e.addEvents({
+                    "mouseover": this.onHandler.bind(this),
+                    "mouseout": this.outHandler.bind(this)
+                });
+            }, this);
         },
         onHandler: function(ev) {
-            ev.target.get("tween", {
-                "property": "opacity",
-                "duration": this.getOption("duration")
-            }).start(this.getOption("opacity"));
-            this.getOption("onHover").apply("",arguments);
+            this.options.onHover.bind(ev.target);
+            this._doTween(ev.target, this.options.opacity);
         },
         outHandler: function(ev) {
-            ev.target.get("tween", {
+            this.options.onOut.bind(ev.target);
+            this._doTween(ev.target, 1);
+        },
+        _doTween: function(el, o) {
+            el.get("tween", {
                 "property": "opacity",
-                "duration": this.getOption("duration")
-            }).start(1);
-            this.getOption("onOut").apply("",arguments);
+                "onComplete": this.options.onComplete,
+                "duration": this.options.duration
+            }).start(o);
         }
     });
 
 
     /**
-     * monogusa Opacity Rollover class Object
-     *
+     * monogusa Konami command class Object
      */
     var monogusaRollImage = new Class({
-        Extends: monogusaAbstract,
-        _options: {
-            suffix: "_on",
-            onHover: $empty,
-            onOut: $empty
-        },
         initialize: function(e, o) {
-            var t = this;
-            this.parent(e, o);
-            this.getElement().each(function(el, i){
-                el.addEvent("mouseover", function(ev){
-                    t.onHandler(ev);
-                });
-                el.addEvent("mouseout", function(ev){
-                    t.outHandler(ev);
-                });
+            this.elements = e;
+            this.options  = $extend(o || {}, {
+                "suffix": "_on",
+                "onHover": $empty,
+                "onOut": $empty
+            });
+
+            this.elements.addEvents({
+                "mouseover": this.onHandler.bind(this),
+                "mouseout": this.outHandler.bind(this)
             });
         },
         onHandler: function(ev) {
-            var src = ev.target.getProperty("src"),
-                tmp = src.split(".");
-            var ext = tmp.pop();
-            src = tmp.join(".") + this.getOption("suffix") + "." + ext;
-            ev.target.setProperty("src", src);
-
+            this.options.onHover.bind(ev.target);
+            this._dispatch(ev.target);
         },
         outHandler: function(ev) {
-            var el = ev.target;
-            var src = el.getProperty("src");
-            el.setProperty("src", src.replace(new RegExp(this.getOption("suffix")), ""));
-            
+            this.options.onOut.bind(ev.target);
+            this._dispatch(ev.target);
+        },
+        _dispatch: function(el) {
+            var base_src = el.getProperty("src"),
+                src, tmp, ext, exp = new RegExp(this.options.suffix);
+
+            if(exp.test(base_src)) {
+                src = base_src.replace(exp, "");
+            }
+            else {
+                tmp = base_src.split(".");
+                ext = tmp.pop();
+                src = tmp.join(".") + this.options.suffix + "." + ext;
+            }
+            el.setProperty("src", src);
         }
     });
 
 
     /**
-     * monogusa Ticker class Object
+     * monogusa Konami command class Object
      */
     var monogusaTicker = new Class({
-        Extends: monogusaAbstract,
-        _options: {
-            speed: 2,
-            pause: 2,
-            onComplete: $empty,
-            onNext: $empty
-        },
-        current_item: -1,
-        items: [],
-        morpth: null,
+        current_item: null,
+        current: -1,
+        items:[],
         initialize: function(e, o) {
-            if($type(e) == "array")
-                e = e[0];
-
-            this.parent(e, o);
-            this.getElement().getParent("div").setStyles({
-                position: "relative"
-            });
-            this.getElement().setStyles({
-                position: "absolute"
+            this.element = e;
+            this.parent_elem = e.getParent();
+            this.options = $extend(o || {}, {
+                speed: 2,
+                duration: 2,
+                onComplete: $empty
             });
 
-            this.items = this.getElement().getElements("li");
-            this.morph = new Fx.Morph(this.getElement(), {
-                duration: this.getOption("speed") * 1000,
-                onComplete: function() {
-                    var i = (this.current_item === 0) ? this.items.length : this.current_item;
-                    this.items[i - 1].injectInside(this.getElement());
-                    this.getElement().setStyles({
+            this.parent_elem.setStyle("position", "relative");
+            this.element.setStyle("position", "absolute");
+
+            this.items = this.element.getChildren();
+            this.element.set("morph", {
+                duration: this.options.speed * 1000,
+                onComplete: function(){
+                    var i = (this.current === 0) ? this.items.length : this.current;
+                    this.items[ i - 1].inject(this.element);
+                    this.element.setStyles({
                         top: 0,
                         left: 0
                     });
-                    this.getOption("onComplete").apply("", arguments);
                 }.bind(this)
             });
             this.next();
         },
         next: function() {
-            ++this.current_item;
-            this.getOption("onNext").call(this.current());
+            ++this.current;
             this.rewind();
-            var pos = this.items[this.current_item];
-            this.morph.start({
+            var pos = this.items[this.current];
+            var morph = this.element.get("morph");
+            morph.start({
                 top: -pos.offsetTop,
                 left: -pos.offsetLeft
             });
-            this.next.bind(this).delay(this.getOption("pause") * 1000 + this.getOption("speed") * 1000);
+            this.next.bind(this).delay(this.options.speed * 1000 + this.options.duration * 1000);
         },
         rewind: function() {
-            if(this.items.length <= this.current_item) {
-                this.current_item = 0;
-            }
-        },
-        current: function() {
-            return this.items[this.current_item];
+            if(this.current >= this.items.length)
+                this.current = 0;
         }
     });
 
 
     /**
-     * monogusa Blank class Object
+     * monogusa Konami command class Object
      */
     var monogusaBlank = new Class({
-        Extends: monogusaAbstract,
         initialize: function(e, o) {
-            this.parent(e, o);
+            this.elements = e;
+            this.options  = $extend(o || {}, {
+                toolbar:     "yes",
+                location:    "yes",
+                directories: "yes",
+                status:      "yes",
+                menubar:     "yes",
+                scrollbar:   "yes",
+                resizable:   "yes",
+                close:       "yes"
+            });
+
+            this.elements.each(function(el, i){
+                var href = el.getProperty("href").split(/\?/);
+                el.setProperty("href", href[0]);
+                el.store("query_string", this._parseQuery(href[1]));
+                el.addEvent("click", this.clickHandler.bind(this));
+            }, this);
+        },
+        clickHandler: function(ev) {
+            var params = $merge(this.options, ev.target.retrieve("query_string")),
+                param = "";
+
+            for(var i in params) {
+                param += i + "=" + params[i] + ",";
+            }
+
+            param = param.substr(0, param.length - 1);
+            window.open(ev.target.getProperty("href"), "monogusa_blank_window", param);
+            return false;
+        },
+        _parseQuery: function(query) {
+            var result = {},
+                params = [],
+                i;
+
+            if(!query) return result;
+
+            params = query.split(/[;&]/);
+            if(params.length <= 0) {
+                return result;
+            }
+
+            for(i = 0; i < params.length; i++) {
+                var param = params[i].split(/[=]/);
+                result[param[0]] = param[1];
+            }
+
+            return result;
         }
     });
 
 
     /**
-     * monogusa Accordion class Object
+     * monogusa Konami command class Object
      */
     var monogusaAccordion = new Class({
-        Extends: monogusaAbstract,
         initialize: function(e, o) {
-            this.parent(e, o);
         }
     });
 
 
     /**
-     * monogusa Tab class Object
+     * monogusa Konami command class Object
      */
     var monogusaTab = new Class({
-        Extends: monogusaAbstract,
+        tabs: [],
+        panels: [],
         initialize: function(e, o) {
-            this.parent(e, o);
+            this.element = e;
+            this.options = $extend(o || {}, {
+                active_class: "active"
+            });
+
+            this.tabs   = this.element.getElements("li")[0];
+            this.tabs.each(function(e, i){
+                var h = e.getElement("a").getProperty("href");
+                this.panels.push(this.element.getElement(h)[0].setStyle("display", "none"));
+            }, this);
+
+            this.tabs.addEvents({
+                "click": this.clickHandler.bind(this)
+            });
+        },
+        clickHandler: function(ev) {
+            var target = ev.target;
+            this.panels.each(function(el, i){
+                el.setStyle("display", "none").addClass("hide").removeClass("show");
+            }, this);
+            $$(target.getProperty("href")).setStyle("display", "block").removeClass("hide").addClass("show");
+            return false;
         }
     });
 
 
     /**
-     * monogusa Stripe class Object
+     * monogusa Konami command class Object
      */
     var monogusaStripe = new Class({
-        Extends: monogusaAbstract,
         initialize: function(e, o) {
-            this.parent(e, o);
         }
     });
 
 
     /**
-     * monogusa Highlight class Object
+     * monogusa Konami command class Object
      */
     var monogusaHighlight = new Class({
-        Extends: monogusaAbstract,
         initialize: function(e, o) {
-            this.parent(e, o);
         }
     });
 
 
     /**
-     * monogusa FontSize Switcher class Object
+     * monogusa Konami command class Object
      */
     var monogusaFswitch = new Class({
-        Extends: monogusaAbstract,
         initialize: function(e, o) {
-            this.parent(e, o);
         }
     });
 
 
     /**
-     * monogusa Scroller class Object
+     * monogusa Konami command class Object
      */
     var monogusaScroller = new Class({
-        Extends: monogusaAbstract,
         initialize: function(e, o) {
-            this.parent(e, o);
         }
     });
 
 
     /**
-     * monogusa Google Maps class Object
+     * monogusa Konami command class Object
      */
     var monogusaGmap = new Class({
-        Extends: monogusaAbstract,
         initialize: function(e, o) {
-            this.parent(e, o);
         }
     });
 
@@ -272,9 +282,7 @@ var monogusa;
      * monogusa Konami command class Object
      */
     var monogusaKonami = new Class({
-        Extends: monogusaAbstract,
         initialize: function(e, o) {
-            this.parent(e, o);
         }
     });
 
