9868bd8ceae1800a989781f0c1fb0df1a7674d12
Nully <nully.nl@gmail.com>
2011-03-28 17:20:26 +0900

    add konami commands plugin

--- src/js/jquery.laquu.js
+++ src/js/jquery.laquu.js
@@ -352,6 +352,42 @@ if(!laquu) var laquu = jQuery.sub();
     };
 
 
+    /**
+     * Konami command
+     *  paramaters
+     *    cmd: command key paramaters.
+     *    callback: command complation callback method.
+     *
+     * Special thanks.
+     *   meltdown js.
+     *   KAZUMiX: http://d.hatena.ne.jp/KAZUMiX/20071105/meltdown
+     */
+    laquu.fn.konami = function(settings) {
+        var defaults = {
+            cmd: "38,38,40,40,37,39,37,39,66,65",
+            callback: function() {
+                var s = document.createElement("script");
+                s.charset="UTF-8";
+                var da = new Date();
+                s.src="http://www.rr.iij4u.or.jp/~kazumix/d/javascript/meltdown/meltdown.js?"+da.getTime();
+                document.body.appendChild(s)
+            }
+        };
+
+        return this.each(function(){
+            var stack = [],
+                opts = $.extend({}, defaults, settings || {});
+            $(this).bind("keydown", function(ev){
+                stack.push(ev.keyCode);
+                if(stack.toString().indexOf(opts.cmd) >= 0) {
+                    $(this).unbind("keydown");
+                    opts.callback.call(this, this, ev, stack);
+                }
+            });
+        });
+    };
+
+
     // Global object
     $.laquu = $.l = {
         version: 1.0,
