37fd14cd027e03e76fc2df8933cb7df00e5bfed2
Nully <nully.nl@gmail.com>
2011-05-27 11:18:08 +0900

    add plugin breadcrumb

--- src/js/jquery.laquu.js
+++ src/js/jquery.laquu.js
@@ -142,6 +142,118 @@ if(!laquu) var laquu = jQuery.sub();
 
 
     /**
+     * breadcrumb plugin
+     * simple animation for breadcrumb.
+     * paramaters
+     *   timer: interval time number
+     *   animTime: crumb animation timer
+     *   isVertical: animation type vertical, or holizontal
+     *   not:
+     *   lastItemClass: crumb last item class name
+     *   easing: easing type (required jQuery.easing plugin)
+     *   onComplete: animation complate callback
+     *   onStep: animation step callback
+     */
+    laquu.fn.breadcrumb = function(options) {
+        var defaults = {
+            timer: 100,
+            animTime: 50,
+            isVertical: false,
+            not: null,
+            lastItemClass: "breadcrumb-last",
+            easing: null,
+            onComplete: $.laquu.empty,
+            onStep: $.laquu.empty
+        };
+
+        return this.each(function(){
+            var o = $.extend({}, defaults, options || {}),
+                _this = $(this),
+                items = _this.children(),
+                itemSize = items.size(),
+                current = 1,
+                timer = null,
+                isStop = false;
+
+                _this.css({
+                    position: "relative",
+                    height: _this.innerHeight(),
+                    overflow: "hidden"
+                });
+
+                // set children item css
+                items.filter(":not(" + o.not + ")").each(function(i, e) {
+                    var p = {
+                        position: "absolute", top: 0, left: 0, zIndex: itemSize - i
+                    },
+                    self = $(this);
+
+                    if(o.isVertical && self.prev().size() != 0) {
+                        var left = self.prev().css("left").replace("px", "");
+                        p.left = self.prev().outerWidth({ margin: true }) + parseInt(left);
+                        p.top = "-" + _this.outerHeight() + "px";
+                    }
+                    self.addClass("breadcrumb-items breadcrumb-item"+i).css(p);
+
+                    return this;
+                }).not(":first").hide();
+
+                items.last().addClass(o.lastItemClass);
+
+                if(o.timer < 50)
+                    o.timer = 50;
+
+                // reset interval timer.
+                o.timer = o.timer + o.animTime;
+
+                timer = setInterval(function(){
+                    var params = opts = {},
+                        current_item = $(items.get(current - 1));
+
+                    params.queue = false;
+                    params.top = 0;
+                    opts.duration = o.animTime;
+
+                    if(o.isVertical) {
+                        isStop = (current - itemSize);
+                    }
+                    else {
+                        var beforeLeft = current_item.prev().css("left");
+                        if(typeof beforeLeft == "undefined")
+                            beforeLeft = "0px";
+
+                        beforeLeft = parseInt(beforeLeft.replace("px", ""));
+                        params.left = current_item.prev().outerWidth({ margin: true }) + beforeLeft;
+                        current_item.css("left", beforeLeft);
+                        isStop = (current > itemSize - 1);
+                    }
+
+                    opts.complete = function() {
+                        if(o.isVertical)
+                            current_item.css("top", params.top);
+                        else
+                            current_item.css("left", params.left);
+
+                        if($.isFunction(o.onComplete))
+                            o.onComplete.apply(current_item);
+                    };
+
+                    if($.isFunction(o.onStep))
+                        opts.onStep = o.onStep;
+
+                    if($.easing && o.easing && $.easing[o.easing])
+                        opts.easing = o.easing;
+
+                    current_item.show().animate(params, opts);
+
+                    ++current;
+                    if(isStop) clearInterval(timer);
+                }, o.timer);
+        });
+    };
+
+
+    /**
      * Bubblepopup plugin
      *
      * paramaters
