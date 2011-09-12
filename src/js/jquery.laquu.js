772f432def50b0f0e415501f27706d1ce76b96b6
Nully <nully.nl@gmail.com>
2011-09-12 11:35:56 +0900

    add innerslide plugin.

--- src/js/jquery.laquu.js
+++ src/js/jquery.laquu.js
@@ -616,6 +616,89 @@ laquu.error = function(msg) {
 
 
 /**
+ * Innerslide plugin
+ *  paramaters
+ *
+ */
+(function($l){
+    $l.fn.innerSlide = function(options) {
+        var defaults = {
+            innerSlideClass: ".laquu-innerslide",
+            showSpeed:300,
+            hideSpeed:200,
+            easing:"swing",
+            slideIn:"bottom",
+            onShowComplete: $l.empty,
+            onHideComplete: $l.empty
+        };
+
+        return this.each(function() {
+            var t = $l(this), slider, sliderCss, o = $l.extend({}, defaults, options || {});
+
+            slider = t.find(o.innerSlideClass);
+            sliderCss = (function(type) {
+                var _over, _out;
+                switch(type) {
+                    case "top":
+                        _over = { top: 0, left:0 };
+                        _out  = { top: parseInt("-" + slider.outerHeight()), left: 0 };
+                        break;
+                    case "right":
+                        _over = { top: 0, right: 0 };
+                        _out  = { top: 0, right: parseInt( "-" + slider.outerWidth()) };
+                        break;
+                    case "bottom":
+                        _over = { top: 0, left: 0 };
+                        _out  = { top: parseInt(slider.outerHeight()), left: 0 }
+                        break;
+                    case "left":
+                        _over = { top: 0, right: 0 };
+                        _out  = { top: 0, right: parseInt(slider.outerWidth()) };
+                        break;
+                    case "top-right":
+                        _over = { top: 0, left: 0 };
+                        _out =  { top: parseInt("-" + slider.outerHeight()), left: parseInt(slider.outerWidth()) };
+                        break;
+                    case "top-left":
+                        _over = { top: 0, left: 0 };
+                        _out =  { top: parseInt("-" + slider.outerHeight()), left: parseInt("-" + slider.outerWidth()) };
+                        break;
+                    case "bottom-right":
+                        _over = { top: 0, right: 0 };
+                        _out =  { top: parseInt(slider.outerHeight()), right: parseInt("-" + slider.outerWidth()) };
+                        break;
+                    case "bottom-left":
+                        _over = { top: 0, right: 0 };
+                        _out =  { top: parseInt(slider.outerHeight()), right: parseInt(slider.outerWidth()) };
+                        break;
+                }
+                return { over: _over, out: _out };
+            })(o.slideIn);
+
+            slider.css(sliderCss.out);
+            t.over({
+                onHover: function(){
+                    slider.stop(true,false).animate(sliderCss.over, {
+                        queue: false,
+                        easing: o.easing,
+                        duration: o.showSpeed,
+                        complete: o.onShowComplete
+                    });
+                },
+                onOut: function() {
+                    slider.stop(true,false).animate(sliderCss.out, {
+                        queue: false,
+                        easing: o.easing,
+                        duration: o.hideSpeed,
+                        complete: o.onHideComplete
+                    });
+                }
+            });
+        });
+    };
+})(laquu);
+
+/**
  * Konami command
  *  paramaters
  *    cmd: command key paramaters.
