ea1bc18d997feee0ce98a554c85d520ebbb235b5
Nully <nully.nl@gmail.com>
2011-10-31 11:14:27 +0900

    overflowプロパティをオプションで有効・無効化できるように修正

--- src/eqheight.js
+++ src/eqheight.js
@@ -5,12 +5,14 @@
  * @オプション
  */
 (function($l){
-    $l.fn.eqheight = function(min, max){
+    $l.fn.eqheight = function(min, max, enableOverflow){
     	var _min = 120, _max = 300, tmp = 0;
 
 		// サイズのスワップ
 		min = (min ? min: _min);
 		max = (max ? max: _max);
+
+        enableOverflow = (typeof enableOverflow == "undefined") ? true : enableOverflow;
 
     	this.each(function(){
     		var _h = $l(this).outerHeight();
@@ -22,6 +24,9 @@
 		if(min > tmp) tmp = min;
 		if(max < tmp) tmp = max;
 
-		return this.height(tmp).css("overflow", "auto");
+        if(enableOverflow == true)
+            this.css("overflow", "auto");
+
+        return this.height(tmp);
     };
 })(laquu);
